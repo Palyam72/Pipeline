9be34346c059aa324c1bb044d82f6d0f55a33828
ROHITH <pythonchannel250104@gmail.com>
2025-04-10 10:05:50 +0530

    Update missing_data.py

--- missing_data.py
+++ missing_data.py
@@ -1,2 +1,55 @@
+import streamlit as st
+from sklearn.impute import *
+
 def missing_data():
-  pass
+    st.subheader("Pipeline For Imputing Missing Values", border=True)
+    
+    imputers = [
+        'IterativeImputer', 'KNNImputer', 'MissingIndicator', 'SimpleImputer',
+        'ArbitraryNumberImputer', 'EndTailImputer', 'CategoricalImputer',
+        'RandomSampleImputer', 'DropMissingData'
+    ]
+    
+    functions_list = [
+        iterative_imputer,
+        knn_imputer,
+        missing_indicator,
+        simple_imputer,
+        arbitrary_number_imputer,
+        end_tail_imputer,
+        categorical_imputer,
+        random_sample_imputer,
+        drop_missing_data
+    ]
+    
+  selectedImputer=st.selectbox("Select the imputer",imputers)
+  if selectedImputer:
+    fuction_list[imputers.index(selectedImputer)]
+
+# Initialize all imputer functions with pass (to be implemented later)
+def iterative_imputer():
+    pass
+
+def knn_imputer():
+    pass
+
+def missing_indicator():
+    pass
+
+def simple_imputer():
+    pass
+
+def arbitrary_number_imputer():
+    pass
+
+def end_tail_imputer():
+    pass
+
+def categorical_imputer():
+    pass
+
+def random_sample_imputer():
+    pass
+
+def drop_missing_data():
+    pass
