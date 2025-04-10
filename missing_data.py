1139c3166b324f834088caad72d4058876b35705
ROHITH <pythonchannel250104@gmail.com>
2025-04-10 10:34:38 +0530

    Update missing_data.py

--- missing_data.py
+++ missing_data.py
@@ -29,7 +29,7 @@ def missing_data():
 # Initialize all imputer functions with pass (to be implemented later)
 def mean_median_imputer():
     st.markdown("##### Set the params")
-    imputation_method=st.seslectbox("How you want to fill the missing values in numerical columns",['median','mean'])
+    imputation_method=st.selectbox("How you want to fill the missing values in numerical columns",['median','mean'])
     variables=st.multiselect("List of numerical columns to impute",[st.session_state['dataset'].select_dtypes(include=['int8','int32','int64','float8','float32','float64']).columns])
     if st.button("Add To Pipeline",use_container_width=True,type='primary'):
         if imputation_method and variables:
